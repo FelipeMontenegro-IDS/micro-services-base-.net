5b503a532a40ea6b93ffe3982031fef0efd0fb69
Felipe Montenegro <felipe.montenegro@idslatam.com>
2025-01-22 19:23:11 -0500

    - IBaseQueue.cs documentando la interface que usan mis objetos response y request para gestionar las colas a base de micro-servicio y su enum de cola.
    - IQueueFactory.cs documentando la interface que usan mis factories de Queue tanto response como de request para manejar las instancias.

--- Application/Interfaces/Azure/ServicesBus/IBaseQueue.cs
+++ Application/Interfaces/Azure/ServicesBus/IBaseQueue.cs
@@ -2,11 +2,33 @@
 
 namespace Application.Interfaces.Azure.ServicesBus;
 
+/// <summary>
+/// Define la interfaz base para gestionar las colas de un micro-servicio utilizando un enumerator para identificar las colas.
+/// </summary>
+/// <typeparam name="TQueue">El tipo de enumerator que representa las colas del micro-servicio.</typeparam>
 public interface IBaseQueue<TQueue> where TQueue : Enum
 {
+    /// <summary>
+    /// Obtiene o establece el microservicio asociado a la cola.
+    /// </summary>
     public MicroService Microservice { get; set; }
+    
+    /// <summary>
+    /// Obtiene o establece la cola específica del micro-servicio, representada por el enumerator <typeparamref name="TQueue"/>.
+    /// </summary>
     public TQueue Queue { get; set; }
+    
+    /// <summary>
+    /// Determina si una cola específica es válida para el micro-servicio proporcionado.
+    /// </summary>
+    /// <param name="microservice">El micro-servicio al que pertenece la cola.</param>
+    /// <param name="queue">La cola a validar, representada por el enumerator <typeparamref name="TQueue"/>.</param>
+    /// <returns><c>true</c> si la cola es válida para el micro-servicio; de lo contrario, <c>false</c>.</returns>
     bool IsValidQueue(MicroService microservice, TQueue queue);
     
+    /// <summary>
+    /// Obtiene el nombre de la cola como una cadena, en función del micro-servicio y el enumerator de cola configurados.
+    /// </summary>
+    /// <returns>El nombre de la cola como una cadena.</returns>
     string GetQueueName();
 }
--- Application/Interfaces/Azure/ServicesBus/IQueueFactory.cs
+++ Application/Interfaces/Azure/ServicesBus/IQueueFactory.cs
@@ -2,8 +2,23 @@ using Shared.Enums.Queue.Services;
 
 namespace Application.Interfaces.Azure.ServicesBus;
 
+/// <summary>
+/// Define una interfaz base para crear instancias de objetos asociados con colas específicas de un micro-servicio, 
+/// como objetos de solicitud (request) o respuesta (response), para gestionar las colas de manera más eficiente.
+/// </summary>
 public interface IQueueFactory
 {
+    /// <summary>
+    /// Crea una instancia de un objeto que implementa <see cref="IBaseQueue{TQueue}"/> para representar una cola específica 
+    /// de un micro-servicio dado.
+    /// </summary>
+    /// <typeparam name="TQueue">El tipo de enumerator que representa las colas asociadas al micro-servicio.</typeparam>
+    /// <param name="microservice">El micro-servicio al que pertenece la cola.</param>
+    /// <param name="queue">La cola específica, representada por el enumerator <typeparamref name="TQueue"/>.</param>
+    /// <returns>
+    /// Una instancia de <see cref="IBaseQueue{TQueue}"/> que representa la cola configurada para el micro-servicio y 
+    /// el enumerator de cola proporcionados.
+    /// </returns>
     public IBaseQueue<TQueue> Create<TQueue>(MicroService microservice, TQueue queue) where TQueue : Enum;
 
 }
